7dbd51e0f99ecacdc3c4b6734e1362e0fb6b4827
Anashkin Aleksei <109337222+NewalexOA@users.noreply.github.com>
2025-01-26 22:24:00 +0300

    chore: add database initialization scripts
    Added scripts for creating multiple PostgreSQL databases, initializing test database, and waiting for PostgreSQL to be ready

---
+++ docker/wait-for-postgres.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# wait-for-postgres.sh
+
+set -e
+
+host="$1"
+port="$2"
+shift 2
+cmd="$@"
+
+until pg_isready -h "$host" -p "$port" -U postgres; do
+  >&2 echo "PostgreSQL is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "PostgreSQL is up - executing command"
+exec $cmd
