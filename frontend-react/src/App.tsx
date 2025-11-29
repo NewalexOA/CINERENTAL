353ac5ac612366f69e72315053230bdd78cb7c8a
dev.anashkin <dev.anashkin@gmail.com>
2025-11-29 16:39:49 +0000

    feat: implement client and equipment details pages and link them in project view
    - Create ClientDetailsPage showing info and project history
    - Create EquipmentDetailsPage showing equipment specs
    - Update App.tsx with new routes
    - Add navigation links to client and equipment in ProjectDetailsPage

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -3,8 +3,10 @@ import { MainLayout } from './components/layout/MainLayout';
 import { Toaster } from './components/ui/sonner';
 import { CartProvider } from './context/CartContext';
 import ClientsPage from './features/clients/pages/ClientsPage';
+import ClientDetailsPage from './features/clients/pages/ClientDetailsPage';
 import CategoriesPage from './features/categories/pages/CategoriesPage';
 import EquipmentPage from './features/equipment/pages/EquipmentPage';
+import EquipmentDetailsPage from './features/equipment/pages/EquipmentDetailsPage';
 import ProjectsPage from './features/projects/pages/ProjectsPage';
 import NewProjectPage from './features/projects/pages/NewProjectPage';
 import ProjectDetailsPage from './features/projects/pages/ProjectDetailsPage';
@@ -18,13 +20,21 @@ function App() {
         <Routes>
           <Route element={<MainLayout />}>
             <Route path="/" element={<Navigate to="/equipment" replace />} />
+            
             <Route path="/equipment" element={<EquipmentPage />} />
+            <Route path="/equipment/:id" element={<EquipmentDetailsPage />} />
+            
             <Route path="/categories" element={<CategoriesPage />} />
+            
             <Route path="/clients" element={<ClientsPage />} />
+            <Route path="/clients/:id" element={<ClientDetailsPage />} />
+            
             <Route path="/bookings" element={<BookingsPage />} />
+            
             <Route path="/projects" element={<ProjectsPage />} />
             <Route path="/projects/new" element={<NewProjectPage />} />
             <Route path="/projects/:id" element={<ProjectDetailsPage />} />
+            
             <Route path="/scanner" element={<ScannerPage />} />
           </Route>
         </Routes>
